d97f5f224e27380169eae0f60ae02fe2fdef4a4d
madiistvan <63722535+madiistvan@users.noreply.github.com>
2022-01-09 19:25:01 +0100

    _

---
+++ part3/Assets/GameSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using SWNetwork;
+using UnityEngine;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public void OnSpawnerReady(bool alreadyFinishedSceneSetup, SceneSpawner sceneSpawner)
+    {
+        if (!alreadyFinishedSceneSetup)
+        {
+            float positionX = Random.Range(-6.0f, 6.0f);
+            float positionY = Random.Range(-6.0f, 6.0f);
+
+            sceneSpawner.SpawnForPlayer(0, new Vector3(positionX, positionY, 0), Quaternion.identity);
+
+            sceneSpawner.PlayerFinishedSceneSetup();
+        }
+    }
+}
